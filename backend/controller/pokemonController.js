77df6a962e6613758e82eaaeadb3b5ff97cd574d
tdo leipzig <tdo.leipzig@gmail.com>
2024-05-29 12:52:42 +0200

    provide backend api

---
+++ backend/controller/pokemonController.js
@@ -0,0 +1,32 @@
+// import static json file
+import pokemonJson from '../pokedex.json' assert { type: "json" };
+
+// send json data of all pokemons to the client
+export const getAllPokemons = async (req, res) => {
+	res.send(pokemonJson);
+};
+
+// send json data of one specific pokemon to the client
+export const getPokemonById = async (req, res) => {
+	let pokemonId = req.params.id;
+	if(pokemonId > pokemonJson.length) {
+		res.status(404).send('404 - No Pokemon Found');
+	}
+	pokemonJson.map(pokemon => {
+		if(parseInt(pokemon.id) === parseInt(pokemonId)){
+			res.send(pokemon);
+		}
+	})
+};
+
+// send selected json data of one specific pokemon to the client (e.g. name, type or base)
+export const getPokemonInfo = async (req, res) => {
+	let pokemonId = req.params.id;
+	let pokemonInfo = req.params.info;
+	pokemonJson.map(pokemon => {
+		if(parseInt(pokemon.id) === parseInt(pokemonId)){
+			// use bracket notation because pokemonInfo is a variable
+			res.send(pokemon[pokemonInfo]);
+		}
+	})
+};
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import pokefightRouter from './routes/pokefightRouter.js';
+
 const app = express();
 const PORT = 8000;
 
 // Set headers for CORS policy due to localhost environment
 app.use(cors());
-const setHeaders = (res) => {
-	res.setHeader("Access-Control-Allow-Origin", "*")
-	res.setHeader("Access-Control-Allow-Credentials", "true");
-	res.setHeader("Access-Control-Max-Age", "1800");
-	res.setHeader("Access-Control-Allow-Headers", "content-type");
-	res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
-};
-
+app.use(express.json());
+app.use('/pokemon', pokefightRouter);
 app.listen(PORT, () => console.log(`Server is running on PORT:${PORT}`));
