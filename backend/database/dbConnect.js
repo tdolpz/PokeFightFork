dbe9cbd50012ddb647816bfedced93bd29a96b2a
tdo leipzig <tdo.leipzig@gmail.com>
2024-05-30 10:11:40 +0200

    adjust log message in dbConnect.js

--- backend/database/dbConnect.js
+++ backend/database/dbConnect.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
 	try {
 		const connection= await mongoose.connect(process.env.MONGO_URI);
 		console.log('Connected to MongoDB');
-		console.log(connection.connection.db.databaseName);
+		console.log('Database: ' + connection.connection.db.databaseName);
 	} catch (error) {
 		console.log('Connection errror:', error.stack);
 	}
