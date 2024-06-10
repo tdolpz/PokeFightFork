9a9924fa548fc4d822962ed43b83327a6a6b8722
tdo leipzig <tdo.leipzig@gmail.com>
2024-06-10 21:37:44 +0200

    update score view

--- frontend/src/views/Score.jsx
+++ frontend/src/views/Score.jsx
@@ -1,14 +1,32 @@
 import { useNavigate } from "react-router-dom";
+
+import leaderboardfont from "../assets/Leaderboardfont.png";
+import firstPlace from "../assets/FirstPlace.png";
+import secondPlace from "../assets/SecondPlace.png";
+import thirdPlace from "../assets/ThirdPlace.png";
+
+
 function Score() {
 
 	const navigate = useNavigate();
 
 	return (
-		<div>
 
-			<button onClick={() => navigate('/')}>New game</button>
 
-		</div>
+
+
+
+
+
+
+
+
+
+		// <div>
+		//
+		// 	<button onClick={() => navigate('/')}>New game</button>
+		//
+		// </div>
 	)
 }
 
