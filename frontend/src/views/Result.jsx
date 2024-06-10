6dc6ba914255bc2f59c7e916d32eeebdaf9f2ddf
tdo leipzig <tdo.leipzig@gmail.com>
2024-06-10 21:31:05 +0200

    update result view

--- frontend/src/views/Result.jsx
+++ frontend/src/views/Result.jsx
@@ -1,13 +1,26 @@
 import {useNavigate, useLocation} from "react-router-dom";
+import PulseButton from "../components/PulseButton";
+import CardBack from "../components/CardBack";
+import defeatImg from "../assets/defeat.png";
+import victoryImg from "../assets/victory.png";
+import you from "../assets/fontyoublk.png";
+import rival from "../assets/fontrivalblk.png";
 
 function Result() {
 
+	const navigate = useNavigate();
 	const { state } = useLocation();
 	const hand1 = state.hand1;
 	const hand2 = state.hand2;
 
 	console.log(hand1);
 	console.log(hand2);
+
+	const toScoreView = () => {
+		navigate('/arena/score');
+	}
+
+
 
 	let victory, defeat;
 
@@ -22,28 +35,70 @@ function Result() {
 	}
 
 	console.log('victory: ', victory, 'defeat: ', defeat);
-	const navigate = useNavigate();
+
 
 	return (
-		<>
-			<div>RESULT</div>
 
-			<div className="w-full grid grid-cols-2">
+		<div className="w-full h-full flex flex-col justify-center">
 
-				<div className="">
-					<div>{victory === 1 ? 'VICTORY' : 'DEFEAT'}</div>
-					<div>PLAYER 1 (YOU)</div>
+			<div className="grid grid-cols-2 gap-4 w-[70%] mx-auto">
+
+				<div className="flex justify-center">
+					<div className="mx-auto max-w-72 animate-pulse">
+						{victory === 1 ? <img src={victoryImg} alt="#" /> : <img src={defeatImg} alt="#" />}
+					</div>
 				</div>
 
-				<div className="">
-					<div>{victory === 2 ? 'VICTORY' : 'DEFEAT'}</div>
-					<div>PLAYER 2 (COMPUTER)</div>
+				<div className="flex justify-center">
+					<div className="mx-auto max-w-72 animate-pulse">
+						{victory === 2 ? <img src={victoryImg} alt="#" /> : <img src={defeatImg} alt="#" />}
+					</div>
+				</div>
+
+				<div className="flex justify-center">
+					<img src={you} alt="#" className="h-16 mb-4" />
+				</div>
+				<div className="flex justify-center">
+					<img src={rival} alt="#" className="h-16 mb-4" />
+				</div>
+
+				<div className="flex justify-center">
+					<CardBack />
+				</div>
+				<div className="flex justify-center">
+					<CardBack />
 				</div>
 
 			</div>
 
-			<button onClick={() => navigate('/arena/score')}>Click</button>
-		</>
+			<div className="mt-4 flex justify-center">
+				<PulseButton view="result" handleClick={toScoreView} />
+			</div>
+
+
+		</div>
+
+
+		// <>
+		// 	<div>RESULT</div>
+		//
+		// 	<div className="w-full grid grid-cols-2">
+		//
+		// 		<div className="">
+		// 			<div>{victory === 1 ? 'VICTORY' : 'DEFEAT'}</div>
+		// 			<div>PLAYER 1 (YOU)</div>
+		// 		</div>
+		//
+		// 		<div className="">
+		// 			<div>{victory === 2 ? 'VICTORY' : 'DEFEAT'}</div>
+		// 			<div>PLAYER 2 (COMPUTER)</div>
+		// 		</div>
+		//
+		// 	</div>
+		//
+		// 	<button onClick={() => navigate('/arena/score')}>Click</button>
+		// </>
+
 	)
 }
 
