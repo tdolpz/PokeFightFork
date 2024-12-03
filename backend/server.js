dc0d46dec1a899d99fd65ce98544c69367be5a21
tdo leipzig <tdo.leipzig@gmail.com>
2024-12-03 10:11:57 +0100

    update package.json, server.js - add cross-env module, change route-endpoints, enable serving frontend/dist/index.html in production mode

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
-import cors from "cors";
-import express from "express";
+import cors from 'cors';
+import express from 'express';
+import path from "path";
 import "./database/dbConnect.js";
 import {errorHandler} from "./middleware/ErrorHandler.js";
 import mongodbRouter from "./routes/mongodbRouter.js";
@@ -7,13 +8,19 @@ import pokefightRouter from "./routes/pokefightRouter.js";
 
 const app = express();
 const PORT = 8000;
+const __dirname = path.resolve();
 
 // Set headers for CORS policy due to localhost environment
 app.use(cors());
 app.use(express.json());
 
-app.use("/pokemon", pokefightRouter);
-app.use('/player', mongodbRouter);
+app.use("/api/pokemon", pokefightRouter);
+app.use('/api/player', mongodbRouter);
+
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+})
 
 app.use(errorHandler);
 app.listen(PORT, () => console.log(`Server is running on PORT:${PORT}`));
