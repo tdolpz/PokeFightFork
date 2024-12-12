c1a3f15e63ed3df9fd9cabc48a6052fc63627d64
tdo leipzig <tdo.leipzig@gmail.com>
2024-12-12 10:57:29 +0100

    rename root route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,15 @@
 import {Route, Routes} from "react-router-dom";
-import Arena from "./views/Arena.jsx";
 import Fight from "./views/Fight.jsx";
 import Result from "./views/Result.jsx";
 import Score from "./views/Score.jsx";
 import Shuffle from "./views/Shuffle.jsx";
 import Start from "./views/Start.jsx";
+import Init from "./views/Init.jsx";
 
 function App() {
-
 	return (
 		<Routes>
-			<Route path='/' element={<Arena/>}>
+			<Route path='/' element={<Init/>}>
 				<Route path='/start' element={<Start/>}/>
 				<Route path='/shuffle' element={<Shuffle/>}/>
 				<Route path='/fight' element={<Fight/>}/>
