39380e4d4094828d66a77318ddba4a931240c4e6
tdo leipzig <tdo.leipzig@gmail.com>
2024-12-12 10:57:59 +0100

    add gameWon conditional state

--- frontend/src/utils/ContextProvider.jsx
+++ frontend/src/utils/ContextProvider.jsx
@@ -10,9 +10,9 @@ export const ContextProvider = ({children}) => {
 	const [playerData, setPlayerData] = useState(null);
 	const [openCard1, setOpenCard1] = useState(null);
 	const [openCard2, setOpenCard2] = useState(null);
+	const [gameWon, setGameWon] = useState(false);
 
-	useEffect(() => {
-	}, []);
+	useEffect(() => {}, []);
 
 	const values = {
 		playerData,
@@ -21,12 +21,14 @@ export const ContextProvider = ({children}) => {
 		openCard1,
 		openCard2,
 		playerId,
+		gameWon,
 		setPlayerData,
 		setPlayerName,
 		setPlayerId,
 		setPokemonData,
 		setOpenCard1,
-		setOpenCard2
+		setOpenCard2,
+		setGameWon
 	};
 
 	return <Context.Provider value={values}>{children}</Context.Provider>;
